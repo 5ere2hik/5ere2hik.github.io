c7d223e3b6a0e8caf34c691027feaeca754ce102
5ere2hik <papkosergey@gmail.com>
2016-10-11 09:54:32 +0300

    При ресайзе остаются только обложки альбомов

---
+++ splean/scripts/script.js
@@ -0,0 +1,34 @@
+var pageIsSmall;
+
+if(window.innerWidth <= 991)
+  pageIsSmall = true;
+else
+  pageIsSmall = false;
+
+var $covers = $(".cover");
+var $smallCovers;
+  
+$(window).resize(function(){
+  
+  if(window.innerWidth <= 991){
+    if(!pageIsSmall){
+      pageIsSmall = true;
+      // $smallCovers = $covers.clone();
+      
+      $(".cover").clone().appendTo("#first-row").addClass("smallCover").removeClass("cover");
+      $(".cover").hide();
+      $("hr:nth-child(n+3)").hide();
+      // $(".well").css("background-color", "red");
+      // console.log(String(pageIsSmall));
+    }
+  } else {
+    if(pageIsSmall){
+      pageIsSmall = false;
+      $(".smallCover").remove();
+      $(".cover").show();
+      $("hr:nth-child(n+3)").show();
+      // $(".well").css("background-color", "green");
+      // console.log(String(pageIsSmall));
+    }
+  }
+});

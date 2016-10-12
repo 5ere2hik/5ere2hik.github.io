6a19a785aa240d9a4b9a74538d97bf2ddf66f3c5
5ere2hik <papkosergey@gmail.com>
2016-10-12 15:58:06 +0300

    Добавлено отображение плиткой на маленьких дисплеях

--- splean/scripts/script.js
+++ splean/scripts/script.js
@@ -1,36 +1,37 @@
 "use strict";
-
-var pageIsSmall;
-
-if(window.innerWidth <= 991)
-  pageIsSmall = true;
-else
-  pageIsSmall = false;
-
-var $covers = $(".cover");
-var $smallCovers;
-  
-$(window).resize(function(){
+$(document).ready(function(){
   
   if(window.innerWidth <= 991){
-    if(!pageIsSmall){
-      pageIsSmall = true;
-      // $smallCovers = $covers.clone();
-      
-      $(".cover").clone().appendTo("#first-row").addClass("smallCover").removeClass("cover");
-      $(".cover").hide();
-      $("hr:nth-child(n+3)").hide();
-      // $(".well").css("background-color", "red");
-      // console.log(String(pageIsSmall));
+    $(".cover").appendTo("#first-row");
     }
-  } else {
-    if(pageIsSmall){
-      pageIsSmall = false;
-      $(".smallCover").remove();
-      $(".cover").show();
-      $("hr:nth-child(n+3)").show();
-      // $(".well").css("background-color", "green");
-      // console.log(String(pageIsSmall));
-    }
-  }
 });
+// else
+  // pageIsSmall = false;
+
+// var $covers = $(".cover");
+// var $smallCovers;
+  
+// $(window).resize(function(){
+  
+  // if(window.innerWidth <= 991){
+    // if(!pageIsSmall){
+      // pageIsSmall = true;
+      // // $smallCovers = $covers.clone();
+      
+      // $(".cover").clone().appendTo("#first-row").addClass("smallCover").removeClass("cover");
+      // $(".cover").hide();
+      // $("hr:nth-child(n+3)").hide();
+      // // $(".well").css("background-color", "red");
+      // // console.log(String(pageIsSmall));
+    // }
+  // } else {
+    // if(pageIsSmall){
+      // pageIsSmall = false;
+      // $(".smallCover").remove();
+      // $(".cover").show();
+      // $("hr:nth-child(n+3)").show();
+      // // $(".well").css("background-color", "green");
+      // // console.log(String(pageIsSmall));
+    // }
+  // }
+// });
